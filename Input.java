c6ce779353aa17e6d7780196ff0ecf5d1e4752eb
Akash Gupta <130357593+akashgupta2233@users.noreply.github.com>
2024-01-15 21:39:25 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+public static void main(string[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter username");
+
+String userName= sc.nextline();
+System.out.println("Username is: " + usrName);
+  }
+}
