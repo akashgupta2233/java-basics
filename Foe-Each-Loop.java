f46787da3e25b602540efc8c210486887d933508
Akash Gupta <130357593+akashgupta2233@users.noreply.github.com>
2024-01-16 11:39:44 +0530

    Create Foe-Each-Loop.java

---
+++ Foe-Each-Loop.java
@@ -0,0 +1,9 @@
+import java .io.*;
+class Loop{
+  public static void main(String[] args){
+    String arr[]={"Ganesh" , "Mushak" , "Ram" , "Shyam" , "Radha"};
+    for(String element : arr){
+      System.out.println( "Name = "+ element  );
+    }
+  }
+}
