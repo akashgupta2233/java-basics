119cb62ff8d4129b614c74fea42b41fe44a4ee01
Akash Gupta <130357593+akashgupta2233@users.noreply.github.com>
2024-01-22 23:22:55 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -9,3 +9,10 @@ String userName= sc.nextline();
 System.out.println("Username is: " + usrName);
   }
 }
+
+//To turn String into desirable data type
+ int x =Integer.parseInt("9");
+double doubleValue = 9.7;
+//To turn value into int data type
+int intValue = (int) doubleValue;
+
